1121c9bfc67fbea9551829d6884b7f8638eff805
Kavin1421 <kkavinkumar24@gmail.com>
2023-09-20 08:04:06 +0530

    Day-1==>Basics📜

---
+++ Math.js
@@ -0,0 +1,7 @@
+// let a=10,b=20;
+const add=(a,b)=>(a+b)
+const sub=(a,b)=>(a-b)
+const mul=(a,b)=>(a*b)
+const div=(a,b)=>(a/b)
+
+module.exports={add,sub,mul,div}
---
+++ Server.mjs
@@ -0,0 +1,47 @@
+console.log("Day 1 => Hope Everthing Fine!!!")
+// // console.log(global)
+// // Import MOdules =>
+// const os = require('os')
+// console.log(os.type())
+// console.log(os.version())
+// console.log(os.homedir())
+// console.log(os.hostname())
+
+// console.log("----------------")
+// console.log(__dirname)
+// console.log(__filename)
+// console.log("----------------")
+// const path = require('path')
+// console.log(path.parse(__filename))
+// console.log("----------------")
+// console.log("--Impoting the Methods in Another File--")
+// const math=require('./Math')
+// console.log(math.add(10,20))
+// console.log(math.sub(100,20))
+// console.log(math.mul(1,20))
+// console.log(math.div(100,20))
+// // Another Type
+// const {add,sub,mul,div}=require('./Math')
+// console.log(add(10,20))
+// console.log(sub(100,20))
+// console.log(mul(1,20))
+// console.log(div(100,20))
+// =======>Renmaing to .mjs Coz of Importing those modules like Actual Es6
+console.log("Renmaing to .mjs Coz of Importing those modules like Actual Es6")
+import { readFile } from 'node:fs';
+
+readFile('./File/text.txt', (err, data) => {
+  if (err) throw err;
+  console.log(data);
+  console.log("For Actual Data");
+  console.log(data.toString());
+});
+
+// Another Type Without ToString ==>
+
+readFile('./File/text.txt','utf8',(err, data) => {
+  if (err) throw err;
+  // console.log(data);
+  console.log("Without Two String!!");
+  console.log(data);
+});
